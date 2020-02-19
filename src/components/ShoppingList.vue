079ffb4022f12b483ce9fabb3c37c983e3485820
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-02-19 12:37:43 +0100

    I CAN ADD AND REMOVE TODOS :D

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,10 +1,17 @@
 <template>
-    <div>
-        <ul>
-            <li v-for ="dog in dogs" :key= "dog.id"> {{dog.name}}</li>
+    <div class="listWrapper">
+        <ul class="listItem">
+            <li v-for ="(dog, index) in dogs" :key= "dog.id"> 
+                <div>
+                    {{dog.name}}
+                </div>
+                <div class="removeItem" @click ="removeListItem(index)">
+                    X
+                </div>
+            </li>
         </ul>
         <div>
-        <input type = "text" placeholder = "Buy what?" v-model = "newDog">
+        <input type = "text" placeholder = "Buy what?" v-model = "newDog" v-on:keyup.enter="addDoggo">
         <button>Köp This!</button>
         </div>
     </div>
@@ -13,23 +20,67 @@
 <script>
 export default{
     name:'ShoppingList',
-    data(){
-        
+    data(){   
         return {
-            newDogs: '',
+            newDog: '',
+
+            idForDoggo: 4,
                 dogs: [
                 {'name': 'Ninja',
-                 'id': 1},
+                 'id': 1,},
                 {'name':'Casper',
                 'id': 2},
                 {'name': 'Ozzy',
-                'id': 3}
-                ],       
+                'id': 3},
+                ]       
         }
+
     },
+
+    methods:{
+        addDoggo(){
+            if(this.newDog.trim().length == 0){
+                return
+            }
+            this.dogs.push({
+                name: this.newDog, 
+                id: this.idForDoggo
+                })
+            this.newDog = ''
+            this.idForDoggo ++
+        },
+
+        removeListItem(index){
+            this.dogs.splice(index, 1)
+        }
+    }
 }
 
 
 </script>
 
+<style>
 
+.listWrapper{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+ul{
+    width: 15%;
+    padding:0;
+    margin: 3rem;
+}
+
+li{
+    display:flex;
+    justify-content: space-between;
+    margin-top: 0.5rem;
+
+}
+
+.removeItem{
+    font-weight:bold;
+}
+</style>
